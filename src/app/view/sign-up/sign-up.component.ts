7faefdd7b55b6cfc3fef6705e3b3021a3e6afeda
Julien <julien.mercier-chauve@isen.yncrea.fr>
2019-10-19 17:32:47 +0200

    nav bar css + sign up and sign in page

--- src/app/view/sign-up/sign-up.component.ts
+++ src/app/view/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService} from '../../service/auth.service'
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+  ) { }
 
   ngOnInit() {
   }
+
+  async onSignUp(form: NgForm) {
+    const email = form.value['email'];
+    const name = form.value['name'];
+    const cpassword = form.value['cpassword'];
+    const password = form.value['password'];
+    if (password === cpassword){
+      try{
+        await this.authService.signUp(name, email, password);
+      } catch(e) {
+        console.log(e);
+      }
+    }
+  }
+
 
 }
